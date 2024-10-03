70dbcfa93fd849081e530f0fe6425524398e02c7
natassia-nn <natassianguyen@gmail.com>
2024-10-03 10:17:21 -0700

    notes

---
+++ Notes.java
@@ -0,0 +1,15 @@
+public class Notes
+{
+    public static void main(String[] args)
+    {
+        String mySchool = "WHS";
+        int myGrade = 12;
+        double myGPA = 4.0;
+        boolean isGoingHoco = True;
+
+        int length = 3;
+        int width = 5;
+        System.out.println("The perimeter is " + (2*(length + width)));
+        System.out.println("The area is " + (length*width));
+    }
+}
